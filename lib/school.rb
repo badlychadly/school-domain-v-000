e1ec99c3e8ef570d750300c0833e1fba8890c72f
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-10 00:20:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,11 @@
 class School
+  attr_accessor :name, :roster
+  
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-attr_reader :roster
 
 def add_student(student_name, grade)
    roster[grade] ||= []
